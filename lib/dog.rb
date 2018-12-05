8fc535191ce7158444fdeff0a47cc23cb3b1461f
Azhar Shah <azharshahstl@yahoo.com>
2018-12-05 18:33:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,10 +54,7 @@ class Dog
   end
   
   def self.new_from_db(row)
-    dog = Dog.new(row[0], row[1], row[2])
-    dog.id = row[0] 
-    dog.name = row[1] 
-    dog.breed = row[2]
+    dog = Dog.new(id: row[0], name: row[1], breed: row[2])
     dog
   end
   
@@ -67,8 +64,30 @@ class Dog
     WHERE id = ?
     SQL
     
-    self.new_from_db(DB[:conn].execute(sql, id))
-    
+    row = (DB[:conn].execute(sql, id).flatten)
+    self.new_from_db(row)
   end
   
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
 end
